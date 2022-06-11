8401a7f7cbd2920abd8119cae50eb8775017923d
sirzzang <sirzzang@naver.com>
2022-06-12 02:34:12 +0900

    jni로 osrm을 호출하기 위한 자바 클래스, 헤더 파일 추가 - OsrmRouteDto, OsrmJniUtil java class 작성 - 헤더 파일 작성

---
+++ route/src/main/java/com/eraser/route/service/OsrmJniUtil.java
@@ -0,0 +1,11 @@
+package com.eraser.route.service;
+
+import com.eraser.route.service.dto.OsrmRouteDto;
+
+public class OsrmJniUtil {
+    
+    private native OsrmRouteDto getOsrmRouteResponse(double fromLongitude, double fromLatitude, double toLongitude, double toLatitude);
+
+    // load native
+
+}
---
+++ route/src/main/java/com/eraser/route/service/dto/OsrmRouteDto.java
@@ -0,0 +1,23 @@
+package com.eraser.route.service.dto;
+
+public class OsrmRouteDto {
+    private String code;
+    private String message;
+    private double distance;
+    private double duration;
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+    public double getDuration() {
+        return duration;
+    }
+}
